0e1a6dd310c0d4c11bab17be00db891ab0959665
feiy <feiy@163.com>
2018-06-07 17:19:59 +0800

    客服消息验证服务器

--- application/api/controller/v1/Customer.php
+++ application/api/controller/v1/Customer.php
@@ -11,22 +11,22 @@ namespace app\api\controller\v1;
 
 class Customer
 {
-    public function validParam(){
+    private function validParam(){
         $signature = input('get.signature');
         $timestamp = input('get.timestamp');
         $nonce = input('get.nonce');
         $token = "2b21d0665720fdd4509ee7fad89342";
         $tmpArr = array($token, $timestamp, $nonce);
+
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode($tmpArr);
-        $tmpStr = sha1($tmpStr);
 
+        $tmpStr = sha1($tmpStr);
         if($tmpStr == $signature){
             return true;
         }
         else{
             return false;
         }
-
     }
 }
