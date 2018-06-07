209275c9e22ecb62c628b681c3a713d2779baf63
feiy <feiy@163.com>
2018-06-07 17:45:48 +0800

    客服消息验证服务器

--- application/api/controller/v1/Customer.php
+++ application/api/controller/v1/Customer.php
@@ -12,20 +12,21 @@ namespace app\api\controller\v1;
 class Customer
 {
     private function checkSignature(){
-        $signature = $_GET['signature'];
-        $timestamp = $_GET['timestamp'];
-        $nonce = $_GET['nonce'];
-        $token = "feiy";
-        $tmpArr = array($token, $timestamp, $nonce);
-
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-        if( $tmpStr == $signature ){
-            echo $_GET['echostr'];
-            exit;
-        }else{
-            return false;
-        }
+        echo $_GET['echostr'];
+//        $signature = $_GET['signature'];
+//        $timestamp = $_GET['timestamp'];
+//        $nonce = $_GET['nonce'];
+//        $token = "feiy";
+//        $tmpArr = array($token, $timestamp, $nonce);
+//
+//        sort($tmpArr, SORT_STRING);
+//        $tmpStr = implode( $tmpArr );
+//        $tmpStr = sha1( $tmpStr );
+//        if( $tmpStr == $signature ){
+//            echo $_GET['echostr'];
+//            exit;
+//        }else{
+//            return false;
+//        }
     }
 }
