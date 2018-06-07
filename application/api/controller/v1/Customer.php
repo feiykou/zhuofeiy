e26a7e8d21a83a439a3e7010e878f235ceb59c30
feiy <feiy@163.com>
2018-06-07 17:49:17 +0800

    客服消息验证服务器

--- application/api/controller/v1/Customer.php
+++ application/api/controller/v1/Customer.php
@@ -8,25 +8,42 @@
 
 namespace app\api\controller\v1;
 
-
+traceHttp();
 class Customer
 {
     private function checkSignature(){
-        echo $_GET['echostr'];
-//        $signature = $_GET['signature'];
-//        $timestamp = $_GET['timestamp'];
-//        $nonce = $_GET['nonce'];
-//        $token = "feiy";
-//        $tmpArr = array($token, $timestamp, $nonce);
-//
-//        sort($tmpArr, SORT_STRING);
-//        $tmpStr = implode( $tmpArr );
-//        $tmpStr = sha1( $tmpStr );
-//        if( $tmpStr == $signature ){
-//            echo $_GET['echostr'];
-//            exit;
-//        }else{
-//            return false;
-//        }
+        $signature = $_GET['signature'];
+        $timestamp = $_GET['timestamp'];
+        $nonce = $_GET['nonce'];
+        $token = "feiy";
+        $tmpArr = array($token, $timestamp, $nonce);
+
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        if( $tmpStr == $signature ){
+            echo $_GET['echostr'];
+        }else{
+            return false;
+        }
+    }
+
+    function traceHttp()
+    {
+        logger("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
+        logger("QUERY_STRING:".$_SERVER["QUERY_STRING"]);
+    }
+    function logger($log_content)
+    {
+        if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
+            sae_set_display_errors(false);
+            sae_debug($log_content);
+            sae_set_display_errors(true);
+        }else{ //LOCAL
+            $max_size = 500000;
+            $log_filename = "log.xml";
+            if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
+            file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
+        }
     }
 }
